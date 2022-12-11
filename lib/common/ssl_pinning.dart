c7ad3fb07bc91ae3ee2339ca57f230b2e849df5b
Rendy Feriansyah <pramudita147@gmail.com>
2022-12-11 14:51:32 +0700

    Add ssl certificate

---
+++ lib/common/ssl_pinning.dart
@@ -0,0 +1,24 @@
+import 'package:http/io_client.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:flutter/services.dart';
+
+class SSLPinningClient extends IOClient {
+  @override
+  Future<http.Response> get(Uri url, {Map<String, String>? headers}) async {
+    return await Shared.initializeIOClient().then((value) => value.get(url));
+  }
+}
+
+class Shared {
+  static Future<IOClient> initializeIOClient() async {
+    final ByteData sslCert =
+        await rootBundle.load('certificates/certificate_moviedb.pem');
+    SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
+    securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
+    HttpClient httpClient = HttpClient(context: securityContext);
+    httpClient.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => false;
+    return IOClient(httpClient);
+  }
+}
